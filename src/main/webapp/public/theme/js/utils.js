ba1f646199df86561d39c7bede3649daa68970d7
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-12 03:38:30 +0200

    Simplified fetch functions in utils.js (usage of lambdas)

--- src/main/webapp/public/theme/js/utils.js
+++ src/main/webapp/public/theme/js/utils.js
@@ -19,34 +19,33 @@ setInterval(updateDateTime, 1000);
 /** ----- END OF DATE & TIME UPDATES ----- **/
 
 
-
 /** ----- FETCH FUNCTIONS ----- **/
 export function fetchOrders() {
     return fetch(`http://localhost:8082/api/orders`)
-        .then(function (response) {
+        .then(response => {
             if (response.ok) {
                 return response.json();
             } else {
                 throw new Error("Communication error: GET /api/orders");
             }
-        }).then(function (data) {
+        }).then(data => {
             return data;
-        }).catch(function (error) {
+        }).catch(error => {
             console.log(error);
         });
 }
 
 export function fetchOrderByTableNumber(id) {
     return fetch(`http://localhost:8082/api/orders/${id}`)
-        .then(function (response) {
+        .then(response => {
             if (response.ok) {
                 return response.json();
             } else {
                 throw new Error("Communication error: GET /api/orders");
             }
-        }).then(function (data) {
+        }).then(data => {
             return data;
-        }).catch(function (error) {
+        }).catch(error => {
             console.log(error);
         });
 }
@@ -64,120 +63,105 @@ export function fetchFinalizedOrders(forTakeAway, limit, offset) {
             'Content-Type': 'application/json'
         },
         body: requestBody
-    }).then(function (response) {
+    }).then(response => {
         if (response.ok) {
             return response.json();
         } else {
             throw new Error('Communication error: POST /api/orders/finalized');
         }
-    }).then(function (data) {
+    }).then(data => {
         return data;
-    }).catch(function (error) {
+    }).catch(error => {
         console.log(error);
     });
 }
 
 export function fetchAllResolved() {
     return fetch(`http://localhost:8082/api/orders/resolved`)
-        .then(function (response) {
+        .then(response => {
             if (response.ok) {
                 return response.json();
             } else {
                 throw new Error("Communication error: GET /api/orders/resolved");
             }
-        }).then(function (data) {
+        }).then(data => {
             return data;
-        }).catch(function (error) {
+        }).catch(error => {
             console.log(error);
         });
 }
 
 export function fetchOrderById(id) {
     return fetch(`http://localhost:8082/api/orders/id/${id}`)
-        .then(function (response) {
+        .then(response => {
             if (response.ok) {
                 return response.json();
             } else {
                 throw new Error("Communication error: GET /api/orders/id");
             }
-        }).then(function (data) {
+        }).then(data => {
             return data;
-        }).catch(function (error) {
+        }).catch(error => {
             console.log(error);
         });
 }
 
 export function fetchTakeAwayOrders() {
     return fetch(`http://localhost:8082/api/orders/takeAway`)
-        .then(function (response) {
+        .then(response => {
             if (response.ok) {
                 return response.json();
             } else {
                 throw new Error("Communication error: GET /api/orders/takeAway");
             }
-        }).then(function (data) {
+        }).then(data => {
             return data;
-        }).catch(function (error) {
+        }).catch(error => {
             console.log(error);
         });
 }
 
 export function fetchDineInOrders() {
     return fetch(`http://localhost:8082/api/orders`)
-        .then(function (response) {
+        .then(response => {
             if (response.ok) {
                 return response.json();
             } else {
                 throw new Error("Communication error: GET /api/orders");
             }
-        }).then(function (data) {
+        }).then(data => {
             return data;
-        }).catch(function (error) {
-            console.log(error);
-        });
-}
-
-export function fetchAllResolvedTakeAway() {
-    return fetch(`http://localhost:8082/api/orders/resolved/take-away`)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error("Communication error: GET /api/orders/resolved");
-            }
-        }).then(function (data) {
-            return data;
-        }).catch(function (error) {
+        }).catch(error => {
             console.log(error);
         });
 }
 
 export function fetchFinalizedOrderById(id, forTakeAway) {
     return fetch(`http://localhost:8082/api/orders/finalized/id/${id}/${forTakeAway}`)
-        .then(function (response) {
+        .then(response => {
             if (response.ok) {
                 return response.json();
             } else {
                 throw new Error("Communication error: GET /api/orders/finalized");
             }
-        }).then(function (data) {
+        }).then(data => {
             return data;
-        }).catch(function (error) {
+        }).catch(error => {
             console.log(error);
         });
 }
 
 export function fetchFinalizedOrdersByDate(date, forTakeAway) {
     return fetch(`http://localhost:8082/api/orders/finalized/date/${date}/${forTakeAway}`)
-        .then(function (response) {
+        .then(response => {
             if (response.ok) {
                 return response.json();
             } else {
                 throw new Error("Communication error: GET /api/orders/finalized");
             }
-        }).then(function (data) {
+        }).then(data => {
             return data;
-        }).catch(function (error) {
+        }).catch(error => {
             console.log(error);
         });
 }
