155997a81dbdad388a95c0aa31db9d5ea204e946
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-12 18:36:57 +0200

    Updated boolean fields of Order entity, removed unnecessary things like = false or nullable = false

--- src/main/java/pl/rarytas/rarytas_restaurantside/entity/Order.java
+++ src/main/java/pl/rarytas/rarytas_restaurantside/entity/Order.java
@@ -33,7 +33,7 @@ public class Order {
     @NotNull
     private Restaurant restaurant;
 
-    @Column(name = "order_time", length = 20, nullable = false)
+    @Column(name = "order_time", length = 50, nullable = false)
     @NotNull
     private String orderTime;
 
@@ -47,20 +47,20 @@ public class Order {
     @Column(name = "total_amount")
     private BigDecimal totalAmount;
 
-    @Column(name = "is_paid", nullable = false)
-    private boolean paid = false;
+    @Column(name = "is_paid")
+    private boolean paid;
 
-    @Column(name = "take_away", nullable = false)
-    private boolean forTakeAway = false;
+    @Column(name = "take_away")
+    private boolean forTakeAway;
 
-    @Column(name = "bill_requested", nullable = false)
-    private boolean billRequested = false;
+    @Column(name = "bill_requested")
+    private boolean billRequested;
 
-    @Column(name = "is_resolved", nullable = false)
-    private boolean isResolved = false;
+    @Column(name = "is_resolved")
+    private boolean isResolved;
 
-    @Column(name = "waiter-called", nullable = false)
-    private boolean waiterCalled = false;
+    @Column(name = "waiter_called")
+    private boolean waiterCalled;
 
     @Column(name = "order_number")
     private Integer orderNumber;
