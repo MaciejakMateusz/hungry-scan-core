9447226f36d5f6d3ff7bc565f23b91294bed3df5
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-12 18:37:34 +0200

    Updated OrderRepository, so it uses more JPA instead of native SQL query

--- src/main/java/pl/rarytas/rarytas_restaurantside/repository/OrderRepository.java
+++ src/main/java/pl/rarytas/rarytas_restaurantside/repository/OrderRepository.java
@@ -9,25 +9,25 @@ import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends CustomRepository<Order, Integer> {
-    @Query(value = "SELECT * FROM rarytas_testing.orders WHERE is_paid = false AND take_away = false AND is_resolved = false", nativeQuery = true)
+    @Query(value = "SELECT o FROM Order o WHERE o.paid = false AND o.forTakeAway = false AND o.isResolved = false")
     List<Order> findAllNotPaid();
 
-    @Query(value = "SELECT * FROM rarytas_testing.orders WHERE take_away = true AND is_resolved = false ORDER BY id DESC", nativeQuery = true)
+    @Query(value = "SELECT o FROM Order o WHERE o.forTakeAway = true AND o.isResolved = false ORDER BY o.id DESC")
     List<Order> findAllTakeAway();
 
-    @Query(value = "SELECT * FROM rarytas_testing.orders WHERE is_resolved = true ORDER BY id DESC", nativeQuery = true)
+    @Query(value = "SELECT o FROM Order o WHERE o.isResolved = true ORDER BY o.id DESC")
     List<Order> findAllResolved();
 
-    @Query(value = "SELECT * FROM rarytas_testing.orders WHERE is_resolved = true AND take_away = true LIMIT 50", nativeQuery = true)
+    @Query(value = "SELECT * FROM orders WHERE is_resolved = true AND take_away = true LIMIT 50", nativeQuery = true)
     List<Order> findAllResolvedTakeAwayLimit50();
 
-    @Query(value = "SELECT * FROM rarytas_testing.orders WHERE table_id = :tableNumber ORDER BY id DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM orders WHERE table_id = :tableNumber ORDER BY id DESC LIMIT 1", nativeQuery = true)
     Optional<Order> findNewestOrderByTableNumber(@Param("tableNumber") Integer tableNumber);
 
     boolean existsByRestaurantTable(RestaurantTable restaurantTable);
 
     @Query(value =
-            "SELECT * FROM rarytas_testing.orders " +
+            "SELECT * FROM orders " +
                     "WHERE is_resolved = true " +
                     "AND take_away = :forTakeAway " +
                     "ORDER BY id DESC " +
@@ -37,7 +37,7 @@ public interface OrderRepository extends CustomRepository<Order, Integer> {
                                     @Param("limit") Integer limit,
                                     @Param("offset") Integer offset);
 
-    @Query(value = "SELECT * FROM rarytas_testing.orders WHERE id = :id AND take_away = :forTakeAway", nativeQuery = true)
+    @Query(value = "SELECT o FROM Order o WHERE o.id = :id AND o.forTakeAway = :forTakeAway")
     Optional<Order> findFinalizedById(@Param("id") Integer id,
                                       @Param("forTakeAway") boolean forTakeAway);
 
