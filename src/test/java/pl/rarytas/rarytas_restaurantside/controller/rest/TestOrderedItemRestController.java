f46d001d41e27fa31ca4c98949ecaa18253bb4bf
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-14 15:38:09 +0200

    Created new tests in TestOrderedItemRestController regarding getting ordered item by ID

--- src/test/java/pl/rarytas/rarytas_restaurantside/controller/rest/TestOrderedItemRestController.java
+++ src/test/java/pl/rarytas/rarytas_restaurantside/controller/rest/TestOrderedItemRestController.java
@@ -18,6 +18,7 @@ import pl.rarytas.rarytas_restaurantside.service.interfaces.OrderedItemServiceIn
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
@@ -45,5 +46,19 @@ public class TestOrderedItemRestController {
         String actualOrderedItemJson = result.getResponse().getContentAsString();
         assertEquals("[{\"id\":1,\"menuItem\":{\"id\":3,\"name\":\"Krewetki w tempurze\",\"description\":\"Delikatne krewetki w cieÅ\u009Bcie tempura, podawane z sosem sÅ\u0082odko-kwaÅ\u009Bnym\",\"ingredients\":\"Krewetki, mÄ\u0085ka, jajko, olej roÅ\u009Blinny, sos sÅ\u0082odko-kwaÅ\u009Bny\",\"price\":22.00,\"created\":\"2023-08-02 04:06:24\",\"updated\":null,\"base64Image\":\"empty\",\"available\":true},\"quantity\":2},{\"id\":2,\"menuItem\":{\"id\":3,\"name\":\"Krewetki w tempurze\",\"description\":\"Delikatne krewetki w cieÅ\u009Bcie tempura, podawane z sosem sÅ\u0082odko-kwaÅ\u009Bnym\",\"ingredients\":\"Krewetki, mÄ\u0085ka, jajko, olej roÅ\u009Blinny, sos sÅ\u0082odko-kwaÅ\u009Bny\",\"price\":22.00,\"created\":\"2023-08-02 04:06:24\",\"updated\":null,\"base64Image\":\"empty\",\"available\":true},\"quantity\":2},{\"id\":3,\"menuItem\":{\"id\":3,\"name\":\"Krewetki w tempurze\",\"description\":\"Delikatne krewetki w cieÅ\u009Bcie tempura, podawane z sosem sÅ\u0082odko-kwaÅ\u009Bnym\",\"ingredients\":\"Krewetki, mÄ\u0085ka, jajko, olej roÅ\u009Blinny, sos sÅ\u0082odko-kwaÅ\u009Bny\",\"price\":22.00,\"created\":\"2023-08-02 04:06:24\",\"updated\":null,\"base64Image\":\"empty\",\"available\":true},\"quantity\":2},{\"id\":4,\"menuItem\":{\"id\":2,\"name\":\"Carpaccio z polÄ\u0099dwicy woÅ\u0082owej\",\"description\":\"Cienko pokrojona polÄ\u0099dwica woÅ\u0082owa podana z rukolÄ\u0085, parmezanem i kaparami.\",\"ingredients\":\"PolÄ\u0099dwica woÅ\u0082owa, rukola, parmezan, kapary, oliwa z oliwek\",\"price\":24.50,\"created\":\"2023-08-02 04:06:03\",\"updated\":null,\"base64Image\":\"empty\",\"available\":true},\"quantity\":3},{\"id\":5,\"menuItem\":{\"id\":3,\"name\":\"Krewetki w tempurze\",\"description\":\"Delikatne krewetki w cieÅ\u009Bcie tempura, podawane z sosem sÅ\u0082odko-kwaÅ\u009Bnym\",\"ingredients\":\"Krewetki, mÄ\u0085ka, jajko, olej roÅ\u009Blinny, sos sÅ\u0082odko-kwaÅ\u009Bny\",\"price\":22.00,\"created\":\"2023-08-02 04:06:24\",\"updated\":null,\"base64Image\":\"empty\",\"available\":true},\"quantity\":2}]",
                 actualOrderedItemJson);
+    }
+
+    @Test
+    public void shouldGetByIdFromDB() {
+        OrderedItem orderedItem = orderedItemService.findById(4).orElse(new OrderedItem());
+        //only ordered item with ID=4 has quantity=3
+        assertEquals(3, orderedItem.getQuantity());
+    }
+
+    @Test
+    public void shouldGetByIdFromEndpoint() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api/orderedItems/4")).andReturn();
+        String actualOrderedItemJson = result.getResponse().getContentAsString();
+        assertTrue(actualOrderedItemJson.contains("\"quantity\":3"));
     }
 }
