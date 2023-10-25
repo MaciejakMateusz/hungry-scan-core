6ab3e307e86c8b355c73bdb32711bfa29fae3d68
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-25 18:54:18 +0200

    Created new test class - CategoryServiceTest

---
+++ src/test/java/pl/rarytas/rarytas_restaurantside/service/CategoryServiceTest.java
@@ -0,0 +1,16 @@
+package pl.rarytas.rarytas_restaurantside.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
