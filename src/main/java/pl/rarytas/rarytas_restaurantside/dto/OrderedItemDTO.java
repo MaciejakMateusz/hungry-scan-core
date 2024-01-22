9f5e7d11bb3d182d5ea2d8d522635dde2e30fffd
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-01-22 15:50:05 +0100

    Created OrderedItemDTO to transfer ordered items form active database to archive database

---
+++ src/main/java/pl/rarytas/rarytas_restaurantside/dto/OrderedItemDTO.java
@@ -0,0 +1,13 @@
+package pl.rarytas.rarytas_restaurantside.dto;
+
+import pl.rarytas.rarytas_restaurantside.entity.MenuItem;
+
+public class OrderedItemDTO {
+
+    private Long id;
+
+    private MenuItem menuItem;
+
+    private Integer quantity;
+
+}
