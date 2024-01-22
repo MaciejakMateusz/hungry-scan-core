f0bc50f2b6d53915dafddf3c00739d3c34131f5f
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-01-22 15:49:17 +0100

    Created HistoryOrderedItem entity

---
+++ src/main/java/pl/rarytas/rarytas_restaurantside/entity/archive/HistoryOrderedItem.java
@@ -0,0 +1,33 @@
+package pl.rarytas.rarytas_restaurantside.entity.archive;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import pl.rarytas.rarytas_restaurantside.entity.MenuItem;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@Table(name = "history_ordered_items")
+public class HistoryOrderedItem implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "menu_item_id", referencedColumnName = "id")
+    private MenuItem menuItem;
+
+    @Column(nullable = false)
+    @Min(value = 1, message = "Ilość musi wynosić minimum 1")
+    @NotNull
+    private Integer quantity;
+
+}
