2d57b1bb2e7ba4df9b40692cd2c5e5580208477c
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-01-24 20:29:31 +0100

    Removed unused interface LoginService

--- src/main/java/pl/rarytas/rarytas_restaurantside/service/interfaces/LoginService.java
+++
@@ -1,7 +0,0 @@
-package pl.rarytas.rarytas_restaurantside.service.interfaces;
-
-import pl.rarytas.rarytas_restaurantside.entity.User;
-
-public interface LoginService {
-    boolean isAuthenticated(User user);
-}
