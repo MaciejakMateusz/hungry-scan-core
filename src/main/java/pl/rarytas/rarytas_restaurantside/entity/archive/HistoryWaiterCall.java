dd09306d574280535214de3b20f662d529e9d4ec
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-01-25 08:47:22 +0100

    Created new history entity HistoryWaiterCall

---
+++ src/main/java/pl/rarytas/rarytas_restaurantside/entity/archive/HistoryWaiterCall.java
@@ -0,0 +1,29 @@
+package pl.rarytas.rarytas_restaurantside.entity.archive;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import pl.rarytas.rarytas_restaurantside.entity.Order;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "history_waiter_calls")
+public class HistoryWaiterCall {
+
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Order order;
+
+    @Column(name = "call_time", nullable = false)
+    private LocalDateTime callTime;
+
+    @Column(name = "is_resolved", nullable = false)
+    private boolean isResolved = false;
+
+}
