fbaf264dec170e6244b498e87f266c76034c1b75
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-01-25 20:26:40 +0100

    Changed WaiterCallRepository to find calls by order and by logical field

--- src/main/java/pl/rarytas/rarytas_restaurantside/repository/WaiterCallRepository.java
+++ src/main/java/pl/rarytas/rarytas_restaurantside/repository/WaiterCallRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.rarytas.rarytas_restaurantside.entity.Order;
 import pl.rarytas.rarytas_restaurantside.entity.WaiterCall;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WaiterCallRepository extends JpaRepository<WaiterCall, Integer> {
-    Optional<WaiterCall> findByOrder(Order order);
+    Optional<WaiterCall> findByOrderAndResolved(Order order, boolean isResolved);
+    List<WaiterCall> findAllByOrder(Order order);
 }
