9185dd490aaa213c14059161819a55b7352327dc
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-01-31 17:35:37 +0100

    Created DataTransferServiceImplTest with test cases

---
+++ src/test/java/pl/rarytas/rarytas_restaurantside/service/DataTransferServiceImplTest.java
@@ -0,0 +1,47 @@
+package pl.rarytas.rarytas_restaurantside.service;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import pl.rarytas.rarytas_restaurantside.entity.Order;
+import pl.rarytas.rarytas_restaurantside.entity.archive.HistoryOrder;
+import pl.rarytas.rarytas_restaurantside.service.archive.interfaces.HistoryOrderService;
+import pl.rarytas.rarytas_restaurantside.service.interfaces.DataTransferService;
+import pl.rarytas.rarytas_restaurantside.service.interfaces.OrderService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@TestPropertySource(locations = "classpath:application-test.properties")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class DataTransferServiceImplTest {
+
+    @Autowired
+    private DataTransferService dataTransferService;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private HistoryOrderService historyOrderService;
+
+    @Test
+    @Transactional
+    void shouldArchiveOrder() {
+        Order existingOrder = orderService.findById(4).orElse(new Order());
+        dataTransferService.archiveOrder(existingOrder);
+        HistoryOrder historyOrder = historyOrderService.findById(4L).orElse(new HistoryOrder());
+        assertEquals("card", historyOrder.getPaymentMethod());
+    }
+}
