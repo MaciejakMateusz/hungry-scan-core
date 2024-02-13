395e8e6fe2d421c6080f7497dfe85bf8e9695142
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-02-13 19:03:38 +0100

    Added JWT configuration

--- src/main/java/pl/rarytas/rarytas_restaurantside/SecurityConfig.java
+++ src/main/java/pl/rarytas/rarytas_restaurantside/SecurityConfig.java
@@ -2,15 +2,22 @@ package pl.rarytas.rarytas_restaurantside;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+import pl.rarytas.rarytas_restaurantside.filter.JwtAuthFilter;
 import pl.rarytas.rarytas_restaurantside.utility.LoginSuccessHandler;
 
 import java.util.List;
@@ -19,7 +26,15 @@ import static jakarta.servlet.DispatcherType.ERROR;
 import static jakarta.servlet.DispatcherType.FORWARD;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -33,13 +48,15 @@ public class SecurityConfig {
                         .Builder(handlerMappingIntrospector);
 
 
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf(Customizer.withDefaults())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         http.cors(c -> c.configurationSource(req -> {
             CorsConfiguration corsConfiguration = new CorsConfiguration();
             corsConfiguration.setAllowedMethods(List.of("*"));
             corsConfiguration.setAllowedHeaders(List.of("*"));
             corsConfiguration.addAllowedOrigin("http://localhost:8080");
+            corsConfiguration.addAllowedOrigin("http://localhost:3000");
             corsConfiguration.setAllowCredentials(true);
             return corsConfiguration;
         }));
@@ -77,6 +94,17 @@ public class SecurityConfig {
         http.httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
     }
 
 }
