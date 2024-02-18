4ab2ee23ecbc1dc7ee7f9322328e8560f62d1e24
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-02-18 11:34:46 +0100

    Created BookingRepository

---
+++ src/main/java/pl/rarytas/rarytas_restaurantside/repository/BookingRepository.java
@@ -0,0 +1,16 @@
+package pl.rarytas.rarytas_restaurantside.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pl.rarytas.rarytas_restaurantside.entity.Booking;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+    @Query("SELECT b FROM Booking b WHERE YEAR(b.date) = :year AND WEEK(b.date) = :week")
+    List<Booking> findAllByWeek(@Param("year") int year, @Param("week") int week);
+
+    List<Booking> findAllByDate(LocalDate date);
+}
