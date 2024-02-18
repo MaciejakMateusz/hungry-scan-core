bea99fdd7007fe6b03fa0cf2abf47787d1880368
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-02-18 11:34:05 +0100

    Created capacity, opening time, closing time for Restaurant entity

--- src/main/java/pl/rarytas/rarytas_restaurantside/entity/Restaurant.java
+++ src/main/java/pl/rarytas/rarytas_restaurantside/entity/Restaurant.java
@@ -1,12 +1,15 @@
 package pl.rarytas.rarytas_restaurantside.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Getter
 @Setter
@@ -28,6 +31,19 @@ public class Restaurant {
 
     private LocalDateTime created;
     private LocalDateTime updated;
+
+    @Min(1)
+    @NotNull
+    @Column(nullable = false)
+    private short capacity;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalTime opening;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalTime closing;
 
     @PrePersist
     private void prePersist() {
