40e68a2133feee968c027cd7154a3a12dc8d334f
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-02-24 20:07:32 +0100

    Rebuilt SettingsServiceImpl to take settings from a restaurant specified in environment variables

--- src/main/java/pl/rarytas/rarytas_restaurantside/service/interfaces/SettingsService.java
+++ src/main/java/pl/rarytas/rarytas_restaurantside/service/interfaces/SettingsService.java
@@ -1,12 +1,11 @@
 package pl.rarytas.rarytas_restaurantside.service.interfaces;
 
-import pl.rarytas.rarytas_restaurantside.entity.Restaurant;
 import pl.rarytas.rarytas_restaurantside.entity.Settings;
 
 import java.util.Optional;
 
 public interface SettingsService {
-    Optional<Settings> findByRestaurant(Restaurant restaurant);
+    Optional<Settings> findByRestaurant();
 
     void save(Settings settings);
 }
