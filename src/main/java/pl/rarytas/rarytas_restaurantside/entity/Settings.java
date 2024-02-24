8977946c917a549f41a1287f6d56d0ff09b6d4b2
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-02-24 16:59:30 +0100

    Created Settings entity with basic fields for a good start

---
+++ src/main/java/pl/rarytas/rarytas_restaurantside/entity/Settings.java
@@ -0,0 +1,31 @@
+package pl.rarytas.rarytas_restaurantside.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Table(name = "settings")
+@Entity
+public class Settings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @NotNull
+    @Column(nullable = false)
+    LocalTime openingTime;
+
+    @NotNull
+    @Column(nullable = false)
+    LocalTime closingTime;
+
+    @NotNull
+    @Column(nullable = false)
+    LocalTime bookingDuration;
+}
