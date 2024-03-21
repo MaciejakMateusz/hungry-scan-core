f6e9c9ae93541b5ac538a0d51c5d2c4a4ec52279
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-03-21 21:45:51 +0100

    Removed tests ordering and implemented rollbacks instead in ArchiveDataServiceImpTest

--- src/test/java/pl/rarytas/rarytas_restaurantside/service/ArchiveDataServiceImpTest.java
+++ src/test/java/pl/rarytas/rarytas_restaurantside/service/ArchiveDataServiceImpTest.java
@@ -1,14 +1,13 @@
 package pl.rarytas.rarytas_restaurantside.service;
 
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import pl.rarytas.rarytas_restaurantside.entity.Order;
@@ -31,7 +30,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @TestPropertySource(locations = "classpath:application-test.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ArchiveDataServiceImpTest {
 
     @Autowired
@@ -51,11 +49,12 @@ class ArchiveDataServiceImpTest {
 
     @Test
     @Transactional
+    @Rollback
     void shouldArchiveOrder() throws LocalizedException {
         Order existingOrder = orderService.findById(4L);
         List<WaiterCall> waiterCalls = waiterCallService.findAllByOrder(existingOrder);
         assertFalse(waiterCalls.isEmpty());
-        assertEquals( "2024-01-29T08:41:20.738823", waiterCalls.get(0).getCallTime().toString());
+        assertEquals("2024-01-29T08:41:20.738823", waiterCalls.get(0).getCallTime().toString());
 
         archiveDataService.archiveOrder(existingOrder);
 
@@ -63,7 +62,7 @@ class ArchiveDataServiceImpTest {
         assertEquals("online", historyOrder.getPaymentMethod());
 
         List<HistoryWaiterCall> historyWaiterCalls = historyWaiterCallService.findAllByHistoryOrder(historyOrder);
-        assertEquals( "2024-01-29T08:41:20.738823", historyWaiterCalls.get(0).getCallTime().toString());
+        assertEquals("2024-01-29T08:41:20.738823", historyWaiterCalls.get(0).getCallTime().toString());
     }
 
     @Test
