1bbde9f95359532f0fb5c79772b13e35747b5faa
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-03-22 10:59:32 +0100

    Removed tests ordering and implemented rollbacks instead in OrderedItemServiceImpTest

--- src/test/java/pl/rarytas/rarytas_restaurantside/service/OrderedItemServiceImpTest.java
+++ src/test/java/pl/rarytas/rarytas_restaurantside/service/OrderedItemServiceImpTest.java
@@ -1,12 +1,15 @@
 package pl.rarytas.rarytas_restaurantside.service;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 import pl.rarytas.rarytas_restaurantside.entity.OrderedItem;
 import pl.rarytas.rarytas_restaurantside.exception.LocalizedException;
 import pl.rarytas.rarytas_restaurantside.service.interfaces.OrderedItemService;
@@ -20,21 +23,18 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @TestPropertySource(locations = "classpath:application-test.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class OrderedItemServiceImpTest {
 
     @Autowired
     private OrderedItemService orderedItemService;
 
     @Test
-    @Order(1)
     void shouldFindAll() {
         List<OrderedItem> orderedItems = orderedItemService.findAll();
         assertEquals(4, orderedItems.size());
     }
 
     @Test
-    @Order(2)
     void shouldFindById() throws LocalizedException {
         OrderedItem orderedItem = orderedItemService.findById(1L);
         assertEquals(3, orderedItem.getMenuItem().getId());
@@ -42,13 +42,13 @@ class OrderedItemServiceImpTest {
     }
 
     @Test
-    @Order(3)
     void shouldNotFindById() {
         assertThrows(LocalizedException.class, () -> orderedItemService.findById(321L));
     }
 
     @Test
-    @Order(4)
+    @Transactional
+    @Rollback
     void shouldToggleIsReadyToServe() throws LocalizedException {
         OrderedItem orderedItem = orderedItemService.findById(1L);
         assertFalse(orderedItem.isReadyToServe());
@@ -61,7 +61,6 @@ class OrderedItemServiceImpTest {
         orderedItemService.toggleIsReadyToServe(1L);
         orderedItem = orderedItemService.findById(1L);
         assertFalse(orderedItem.isReadyToServe());
-
     }
 
 }
