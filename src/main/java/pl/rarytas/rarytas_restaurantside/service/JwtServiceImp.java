fe58b79090b86b28c63753dd2420afd8b0f6a44c
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-05-03 13:35:57 +0200

    Upgraded dependencies and adjusted JwtServiceImp to correct deprecated API

--- src/main/java/pl/rarytas/rarytas_restaurantside/service/JwtServiceImp.java
+++ src/main/java/pl/rarytas/rarytas_restaurantside/service/JwtServiceImp.java
@@ -2,7 +2,6 @@ package pl.rarytas.rarytas_restaurantside.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.core.env.Environment;
@@ -10,7 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import pl.rarytas.rarytas_restaurantside.service.interfaces.JwtService;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -59,26 +58,26 @@ public class JwtServiceImp implements JwtService {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignKey())
+        return Jwts.parser()
+                .verifyWith(getSignKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     private String createToken(Map<String, Object> claims, String username, long hoursToExpire) {
         long expirationTimeMillis = System.currentTimeMillis() + (hoursToExpire * 60 * 60 * 1000); // 20 hours
 
         return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(username)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(expirationTimeMillis))
-                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+                .claims(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(expirationTimeMillis))
+                .signWith(getSignKey(), Jwts.SIG.HS256)
+                .compact();
     }
 
-    private Key getSignKey() {
+    private SecretKey getSignKey() {
         byte[] keyBytes = Decoders.BASE64.decode(environment.getProperty("JWT_SECRET"));
         return Keys.hmacShaKeyFor(keyBytes);
     }
