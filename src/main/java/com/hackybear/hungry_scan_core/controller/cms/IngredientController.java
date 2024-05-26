a28b733380f17fb10dce828cbd3b9207d4f2e2ea
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-05-26 12:04:03 +0200

    Changed the way list of ingredients is being fetched. Instead of List the endpoint returns Page

--- src/main/java/com/hackybear/hungry_scan_core/controller/cms/IngredientController.java
+++ src/main/java/com/hackybear/hungry_scan_core/controller/cms/IngredientController.java
@@ -4,6 +4,9 @@ import com.hackybear.hungry_scan_core.controller.ResponseHelper;
 import com.hackybear.hungry_scan_core.entity.Ingredient;
 import com.hackybear.hungry_scan_core.service.interfaces.IngredientService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +14,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -26,10 +28,13 @@ public class IngredientController {
         this.responseHelper = responseHelper;
     }
 
-    @GetMapping
+    @PostMapping
     @PreAuthorize("hasAnyRole('MANAGER', 'ADMIN')")
-    public ResponseEntity<List<Ingredient>> list() {
-        return ResponseEntity.ok(ingredientService.findAll());
+    public ResponseEntity<Page<Ingredient>> list(@RequestBody Map<String, Object> params) {
+        Integer pageSize = (Integer) params.get("pageSize");
+        Integer pageNumber = (Integer) params.get("pageNumber");
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        return ResponseEntity.ok(ingredientService.findAll(pageable));
     }
 
     @PostMapping("/show")
--- src/main/java/com/hackybear/hungry_scan_core/repository/IngredientRepository.java
+++ src/main/java/com/hackybear/hungry_scan_core/repository/IngredientRepository.java
@@ -1,9 +1,15 @@
 package com.hackybear.hungry_scan_core.repository;
 
 import com.hackybear.hungry_scan_core.entity.Ingredient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+
+    @Query("SELECT i FROM Ingredient i ORDER BY i.name.defaultTranslation")
+    Page<Ingredient> findAllOrderByDefaultTranslation(Pageable pageable);
 }
