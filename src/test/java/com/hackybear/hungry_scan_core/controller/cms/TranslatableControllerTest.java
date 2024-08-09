ec851ba07a6a31899f562c4c4319f76ef4daeaf5
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-08-09 21:35:00 +0200

    Adjusted TranslatableControllerTest to match new structure

--- src/test/java/com/hackybear/hungry_scan_core/controller/cms/TranslatableControllerTest.java
+++ src/test/java/com/hackybear/hungry_scan_core/controller/cms/TranslatableControllerTest.java
@@ -1,5 +1,6 @@
 package com.hackybear.hungry_scan_core.controller.cms;
 
+import com.hackybear.hungry_scan_core.entity.MenuItem;
 import com.hackybear.hungry_scan_core.entity.Translatable;
 import com.hackybear.hungry_scan_core.test_utils.ApiRequestUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -17,9 +18,9 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @Slf4j
 @SpringBootTest
@@ -43,53 +44,26 @@ class TranslatableControllerTest {
 
     @Test
     @WithMockUser(roles = {"MANAGER", "ADMIN"})
-    void shouldFindAll() throws Exception {
-        Map<String, List<Translatable>> translatables =
-                apiRequestUtils.fetchTranslatablesMap("/api/cms/translatable");
-        assertEquals(7, translatables.size());
-
-        List<Translatable> menuItemsTransl = translatables.get("menuItems");
-        List<Translatable> ingredientsTransl = translatables.get("ingredients");
-        List<Translatable> categoriesTransl = translatables.get("categories");
-        List<Translatable> variantsTransl = translatables.get("variants");
-        List<Translatable> zonesTransl = translatables.get("zones");
-        List<Translatable> allergensTransl = translatables.get("allergens");
-        List<Translatable> labelsTransl = translatables.get("labels");
-
-        assertEquals(66, menuItemsTransl.size());
-        assertEquals("Delikatne krewetki w cieście tempura, podawane z sosem słodko-kwaśnym", menuItemsTransl.get(5).getDefaultTranslation());
-        assertEquals(27, ingredientsTransl.size());
-        assertEquals("Szpinak", ingredientsTransl.get(17).getDefaultTranslation());
-        assertEquals(9, categoriesTransl.size());
-        assertEquals("Pizza", categoriesTransl.get(4).getDefaultTranslation());
-        assertEquals(17, variantsTransl.size());
-        assertEquals("Z konfiturą cebulową", variantsTransl.get(1).getDefaultTranslation());
-        assertEquals(4, zonesTransl.size());
-        assertEquals("VIP Lounge", zonesTransl.get(3).getTranslationEn());
-        assertEquals(28, allergensTransl.size());
-        assertEquals("Sulfur dioxide", allergensTransl.get(22).getTranslationEn());
-        assertEquals(6, labelsTransl.size());
-        assertEquals("Gluten free", labelsTransl.get(0).getTranslationEn());
-    }
-
-    @Test
-    @WithMockUser(roles = {"MANAGER", "ADMIN"})
     @Transactional
     @Rollback
     void shouldSaveAll() throws Exception {
-        Map<String, List<Translatable>> translatables =
-                apiRequestUtils.fetchTranslatablesMap("/api/cms/translatable");
-        translatables.get("zones").get(2).setTranslationEn("High On Life");
-        translatables.get("categories").get(4).setDefaultTranslation("Pitca");
+        MenuItem menuItem = apiRequestUtils.postObjectExpect200("/api/cms/items/show", 4, MenuItem.class);
+        assertNull(menuItem.getName().getTranslationEn());
+        assertNull(menuItem.getDescription().getTranslationEn());
 
-        apiRequestUtils.postAndExpect200("/api/cms/translatable", translatables);
+        Translatable nameTranslation = menuItem.getName();
+        nameTranslation.setTranslationEn("English name");
 
-        Map<String, List<Translatable>> updatedTranslatables =
-                apiRequestUtils.fetchTranslatablesMap("/api/cms/translatable");
+        Translatable descriptionTranslation = menuItem.getDescription();
+        descriptionTranslation.setTranslationEn("English description");
 
-        assertEquals(7, updatedTranslatables.size());
-        assertEquals("High On Life", translatables.get("zones").get(2).getTranslationEn());
-        assertEquals("Pitca", translatables.get("categories").get(4).getDefaultTranslation());
+        List<Translatable> translatables = List.of(nameTranslation, descriptionTranslation);
+
+        apiRequestUtils.postAndExpect200("/api/cms/translatable/save-all", translatables);
+
+        menuItem = apiRequestUtils.postObjectExpect200("/api/cms/items/show", 4, MenuItem.class);
+        assertEquals("English name", menuItem.getName().getTranslationEn());
+        assertEquals("English description", menuItem.getDescription().getTranslationEn());
     }
 
 }
