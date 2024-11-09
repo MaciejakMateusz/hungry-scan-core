ed7ebf0c49a3722e9078a54ace6d7fcf77c62dc1
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-11-09 08:33:13 +0100

    - implemented Serializable into Category entity and its DTOs

--- src/main/java/com/hackybear/hungry_scan_core/dto/CategoryCustomerDTO.java
+++ src/main/java/com/hackybear/hungry_scan_core/dto/CategoryCustomerDTO.java
@@ -1,11 +1,16 @@
 package com.hackybear.hungry_scan_core.dto;
 
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.List;
 
 public record CategoryCustomerDTO(long id,
                                   TranslatableDTO name,
-                                  List<MenuItemSimpleDTO> menuItems) {
+                                  List<MenuItemSimpleDTO> menuItems) implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public CategoryCustomerDTO(long id,
                                TranslatableDTO name,
