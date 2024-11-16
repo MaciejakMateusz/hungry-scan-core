01455b1f0966e90df42f2a96c5a134d964c3e29f
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-11-16 21:29:28 +0100

    Expanded and reconfigured environmental variables, updated EmailService

--- src/main/java/com/hackybear/hungry_scan_core/service/interfaces/EmailService.java
+++ src/main/java/com/hackybear/hungry_scan_core/service/interfaces/EmailService.java
@@ -3,6 +3,7 @@ package com.hackybear.hungry_scan_core.service.interfaces;
 import com.hackybear.hungry_scan_core.exception.LocalizedException;
 
 public interface EmailService {
+
     void passwordRecovery(String to) throws LocalizedException;
 
     void contactForm(String from, String subject, String text);
