57a3e23fd6cd8d31a2160feca9410f75319e0db2
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-02-18 20:53:04 +0100

    Created new method to find current active menu for a restaurant

--- src/main/java/com/hackybear/hungry_scan_core/repository/MenuRepository.java
+++ src/main/java/com/hackybear/hungry_scan_core/repository/MenuRepository.java
@@ -2,8 +2,12 @@ package com.hackybear.hungry_scan_core.repository;
 
 import com.hackybear.hungry_scan_core.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalTime;
+import java.util.Optional;
 import java.util.Set;
 
 
@@ -11,4 +15,22 @@ import java.util.Set;
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
     Set<Menu> findAllByRestaurantId(Long restaurantId);
+
+    @Query(value = """
+            SELECT s.menu_id
+            FROM schedule s
+            JOIN schedule_plan sp ON s.id = sp.schedule_id
+            JOIN menus m ON s.menu_id = m.id
+            WHERE m.restaurant_id = :restaurantId
+              AND (
+                   (sp.plan_key = :dayOfWeekOrdinal
+                    AND sp.start_time <= :currentTime
+                    AND sp.end_time >= :currentTime
+                   )
+                   OR m.is_all_day = true
+                  )
+            """, nativeQuery = true)
+    Optional<Long> findActiveMenuId(@Param("dayOfWeekOrdinal") int dayOfWeekOrdinal,
+                                    @Param("currentTime") LocalTime currentTime,
+                                    @Param("restaurantId") Long restaurantId);
 }
