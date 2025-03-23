0addec0a98ce05cf66927554ccb6d527f84b9563
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-23 20:08:08 +0100

    Updated MenuMapper

--- src/main/java/com/hackybear/hungry_scan_core/dto/mapper/MenuMapper.java
+++ src/main/java/com/hackybear/hungry_scan_core/dto/mapper/MenuMapper.java
@@ -1,5 +1,6 @@
 package com.hackybear.hungry_scan_core.dto.mapper;
 
+import com.hackybear.hungry_scan_core.dto.MenuDTO;
 import com.hackybear.hungry_scan_core.dto.MenuSimpleDTO;
 import com.hackybear.hungry_scan_core.entity.Menu;
 import org.mapstruct.Mapper;
@@ -7,8 +8,12 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring", uses = {ScheduleMapper.class})
 public interface MenuMapper {
 
-    MenuSimpleDTO toDTO(Menu menu);
+    MenuSimpleDTO toSimpleDTO(Menu menu);
+
+    MenuDTO toDTO(Menu menu);
 
     Menu toMenu(MenuSimpleDTO menuSimpleDTO);
+
+    Menu toMenu(MenuDTO menuDTO);
 
 }
