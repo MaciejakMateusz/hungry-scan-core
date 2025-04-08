f93e41738a44c6d2e8b9661b3b7a328a21cea394
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-04-08 16:00:35 +0200

    Added audit fields to MenuDTO

--- src/main/java/com/hackybear/hungry_scan_core/dto/MenuDTO.java
+++ src/main/java/com/hackybear/hungry_scan_core/dto/MenuDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 
 import java.io.Serializable;
 import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -12,7 +13,11 @@ public record MenuDTO(Long id,
                       @NotBlank String name,
                       List<CategoryDTO> categories,
                       Map<DayOfWeek, TimeRange> plan,
-                      boolean standard) implements Serializable, Comparable<MenuDTO> {
+                      boolean standard,
+                      LocalDateTime created,
+                      LocalDateTime updated,
+                      String modifiedBy,
+                      String createdBy) implements Serializable, Comparable<MenuDTO> {
 
     @Override
     public int compareTo(MenuDTO other) {
