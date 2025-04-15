f5cdc135dcd3ee262bdc35de272094a50ff2677d
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-04-15 21:22:06 +0200

    Updated RedisSerializationTest to include new Restaurant fields

--- src/test/java/com/hackybear/hungry_scan_core/integration/cache/RedisSerializationTest.java
+++ src/test/java/com/hackybear/hungry_scan_core/integration/cache/RedisSerializationTest.java
@@ -6,11 +6,13 @@ import com.hackybear.hungry_scan_core.dto.MenuSimpleDTO;
 import com.hackybear.hungry_scan_core.dto.RestaurantDTO;
 import com.hackybear.hungry_scan_core.dto.SettingsDTO;
 import com.hackybear.hungry_scan_core.enums.Language;
+import com.hackybear.hungry_scan_core.repository.PricePlanRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -37,6 +39,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RedisSerializationTest {
 
+    @Autowired
+    private PricePlanRepository pricePlanRepository;
+
     @Test
     public void testSerialization() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -62,6 +67,8 @@ public class RedisSerializationTest {
                 "Katowice",
                 getMenuSimpleDTOs(),
                 getSettingsDTO(),
+                pricePlanRepository.findById("free").orElseThrow(),
+                null,
                 Instant.now());
     }
 
