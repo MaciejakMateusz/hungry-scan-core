132428fd6a64afb3a79004829742e07f8f86c1aa
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-04-21 08:45:46 +0200

    Corrected ExceptionHelper to use Locale directly in method level

--- src/main/java/com/hackybear/hungry_scan_core/exception/ExceptionHelper.java
+++ src/main/java/com/hackybear/hungry_scan_core/exception/ExceptionHelper.java
@@ -11,24 +11,25 @@ import java.util.function.Supplier;
 public class ExceptionHelper {
 
     private final MessageSource messageSource;
-    private final Locale locale = LocaleContextHolder.getLocale();
-
 
     public ExceptionHelper(MessageSource messageSource) {
         this.messageSource = messageSource;
     }
 
     public void throwLocalizedMessage(String messageCode, Object... args) throws LocalizedException {
+        Locale locale = LocaleContextHolder.getLocale();
         throw new LocalizedException(String.format(messageSource.getMessage(
                 messageCode, args, locale)));
     }
 
     public Supplier<LocalizedException> supplyLocalizedMessage(String messageCode, Object... args) {
+        Locale locale = LocaleContextHolder.getLocale();
         return () -> new LocalizedException(String.format(messageSource.getMessage(
                 messageCode, args, locale)));
     }
 
     public String getLocalizedMsg(String messageCode, Object... args) {
+        Locale locale = LocaleContextHolder.getLocale();
         return String.format(messageSource.getMessage(messageCode, args, locale));
     }
 }
