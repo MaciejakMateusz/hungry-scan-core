dcdeca233049a784bef826f8838f4ec34ee0dc86
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-04-26 15:20:43 +0200

    Updated SettingsController

--- src/main/java/com/hackybear/hungry_scan_core/controller/admin/SettingsController.java
+++ src/main/java/com/hackybear/hungry_scan_core/controller/admin/SettingsController.java
@@ -1,30 +1,24 @@
 package com.hackybear.hungry_scan_core.controller.admin;
 
 import com.hackybear.hungry_scan_core.controller.ResponseHelper;
-import com.hackybear.hungry_scan_core.dto.SettingsDTO;
 import com.hackybear.hungry_scan_core.exception.LocalizedException;
 import com.hackybear.hungry_scan_core.service.interfaces.SettingsService;
-import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/settings")
 @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"})
 @PreAuthorize("hasAnyRole('ADMIN')")
+@RequiredArgsConstructor
 public class SettingsController {
 
     private final SettingsService settingsService;
     private final ResponseHelper responseHelper;
-
-    public SettingsController(SettingsService settingsService, ResponseHelper responseHelper) {
-        this.settingsService = settingsService;
-        this.responseHelper = responseHelper;
-    }
 
     @GetMapping
     public ResponseEntity<?> getSettings() {
@@ -33,11 +27,6 @@ public class SettingsController {
         } catch (LocalizedException e) {
             return responseHelper.createErrorResponse(e);
         }
-    }
-
-    @PatchMapping
-    public ResponseEntity<?> updateSettings(@RequestBody @Valid SettingsDTO settingsDTO, BindingResult br) {
-        return responseHelper.buildResponse(settingsDTO, br, settingsService::save);
     }
 
     @RequestMapping(method = RequestMethod.OPTIONS)
--- src/test/java/com/hackybear/hungry_scan_core/controller/admin/SettingsControllerTest.java
+++ src/test/java/com/hackybear/hungry_scan_core/controller/admin/SettingsControllerTest.java
@@ -1,8 +1,6 @@
 package com.hackybear.hungry_scan_core.controller.admin;
 
 import com.hackybear.hungry_scan_core.dto.SettingsDTO;
-import com.hackybear.hungry_scan_core.dto.mapper.SettingsMapper;
-import com.hackybear.hungry_scan_core.entity.Settings;
 import com.hackybear.hungry_scan_core.enums.Language;
 import com.hackybear.hungry_scan_core.test_utils.ApiRequestUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -14,16 +12,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalTime;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
 @SpringBootTest
@@ -37,9 +31,6 @@ class SettingsControllerTest {
 
     @Autowired
     private ApiRequestUtils apiRequestUtils;
-
-    @Autowired
-    private SettingsMapper settingsMapper;
 
     @Order(1)
     @Sql("/data-h2.sql")
@@ -70,64 +61,5 @@ class SettingsControllerTest {
     @Test
     void shouldNotAllowUnauthorizedAccessToSettings() throws Exception {
         apiRequestUtils.fetchAndExpectForbidden("/api/admin/settings");
-    }
-
-    @Test
-    @WithMockUser(roles = "ADMIN", username = "admin@example.com")
-    @Transactional
-    @Rollback
-    void shouldUpdateSettings() throws Exception {
-        Settings settings = createSettings();
-        SettingsDTO settingsDTO = settingsMapper.toDTO(settings);
-        apiRequestUtils.patchAndExpect200("/api/admin/settings", settingsDTO);
-
-        SettingsDTO updatedSettings =
-                apiRequestUtils.fetchObject(
-                        "/api/admin/settings", SettingsDTO.class);
-        assertEquals(2, updatedSettings.bookingDuration());
-        assertEquals(LocalTime.of(10, 30), updatedSettings.openingTime());
-        assertEquals(LocalTime.of(22, 0), updatedSettings.closingTime());
-        assertEquals(Language.PL, updatedSettings.language());
-        assertEquals((short) 90, updatedSettings.capacity());
-    }
-
-    @Test
-    @WithMockUser(roles = "COOK")
-    void shouldNotAllowAccessToUpdateSettings() throws Exception {
-        Settings settings = createSettings();
-        apiRequestUtils.patchAndExpect("/api/admin/settings", settings, status().isForbidden());
-    }
-
-    @Test
-    void shouldNotAllowUnauthorizedAccessToUpdateSettings() throws Exception {
-        Settings settings = createSettings();
-        apiRequestUtils.patchAndExpectForbidden("/api/admin/settings", settings);
-    }
-
-    @Test
-    @WithMockUser(roles = "ADMIN")
-    void shouldNotUpdateIncorrectSettings() throws Exception {
-        Settings settings = createSettings();
-        settings.setClosingTime(null);
-        settings.setLanguage(null);
-
-        Map<?, ?> errors = apiRequestUtils.patchAndExpectErrors("/api/admin/settings", settings);
-
-        assertEquals(2, errors.size());
-        assertEquals("Pole nie może być puste", errors.get("closingTime"));
-        assertEquals("Pole nie może być puste", errors.get("language"));
-    }
-
-    private Settings createSettings() {
-        Settings settings = new Settings();
-        settings.setId(1L);
-        settings.setCapacity((short) 90);
-        settings.setBookingDuration(2L);
-        settings.setLanguage(Language.PL);
-        settings.setOpeningTime(LocalTime.of(10, 30));
-        settings.setClosingTime(LocalTime.of(22, 0));
-        settings.setCustomerSessionTime(3L);
-        settings.setEmployeeSessionTime(20L);
-        return settings;
     }
 }
