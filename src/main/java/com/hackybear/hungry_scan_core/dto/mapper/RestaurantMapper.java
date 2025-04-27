8313a638ffc54eb5892d4977fc314374aadcfc1f
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-04-27 12:18:07 +0200

    Expanded RestaurantMapper - added updateFromDTO mapping definition

--- src/main/java/com/hackybear/hungry_scan_core/dto/mapper/RestaurantMapper.java
+++ src/main/java/com/hackybear/hungry_scan_core/dto/mapper/RestaurantMapper.java
@@ -3,15 +3,21 @@ package com.hackybear.hungry_scan_core.dto.mapper;
 import com.hackybear.hungry_scan_core.dto.RestaurantDTO;
 import com.hackybear.hungry_scan_core.dto.RestaurantSimpleDTO;
 import com.hackybear.hungry_scan_core.entity.Restaurant;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", uses = {SettingsMapper.class})
 public interface RestaurantMapper {
 
     RestaurantDTO toDTO(Restaurant restaurant);
 
-    RestaurantSimpleDTO toSimpleDTO(Restaurant restaurant);
-
     Restaurant toRestaurant(RestaurantDTO restaurantDTO);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromDTO(RestaurantDTO restaurantDTO, @MappingTarget Restaurant restaurant);
+
+    RestaurantSimpleDTO toSimpleDTO(Restaurant restaurant);
 
 }
