d2277e11f070e114d36b05f7968b886216ac8b12
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-02 08:43:20 +0200

    Simplified TimeRange embeddable

--- src/main/java/com/hackybear/hungry_scan_core/utility/TimeRange.java
+++ src/main/java/com/hackybear/hungry_scan_core/utility/TimeRange.java
@@ -20,16 +20,8 @@ public class TimeRange implements Serializable {
     private static final long serialVersionUID = 1L;
 
     public TimeRange(LocalTime startTime, LocalTime endTime) {
-        if (startTime.isAfter(endTime)) {
-            throw new IllegalArgumentException("Start time must be before end time");
-        }
         this.startTime = startTime;
         this.endTime = endTime;
-    }
-
-    public boolean includes(TimeRange other) {
-        return !this.startTime.isAfter(other.getStartTime()) &&
-                !this.endTime.isBefore(other.getEndTime());
     }
 
     @Override
