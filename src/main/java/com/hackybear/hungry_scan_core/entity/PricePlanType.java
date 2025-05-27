1d99ac84b6094a5f49b0b205bdbf9dad03b7aa41
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-27 17:22:14 +0200

    Created PricePlanType

---
+++ src/main/java/com/hackybear/hungry_scan_core/entity/PricePlanType.java
@@ -0,0 +1,34 @@
+package com.hackybear.hungry_scan_core.entity;
+
+import com.hackybear.hungry_scan_core.utility.Money;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Table(name = "price_plan_types")
+@Entity
+public class PricePlanType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Column(nullable = false)
+    @NotNull
+    private BigDecimal price = Money.of(0.0);
+
+}
