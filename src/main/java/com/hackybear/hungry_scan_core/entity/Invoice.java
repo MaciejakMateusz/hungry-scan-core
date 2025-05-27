b767fd7037362d51504b220b7db50cffc5e9cc71
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-27 17:27:38 +0200

    Created Invoice entity

---
+++ src/main/java/com/hackybear/hungry_scan_core/entity/Invoice.java
@@ -0,0 +1,35 @@
+package com.hackybear.hungry_scan_core.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Table(name = "price_plans")
+@Entity
+public class Invoice implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    @JsonIgnore
+    private Restaurant restaurant;
+
+    private String name;
+
+}
