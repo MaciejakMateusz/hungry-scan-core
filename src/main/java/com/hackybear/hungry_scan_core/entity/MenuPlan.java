ef322ec070416f00767e79fa2cfed33b8aa3478e
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-06-02 16:00:19 +0200

    Created MenuPlan entity

---
+++ src/main/java/com/hackybear/hungry_scan_core/entity/MenuPlan.java
@@ -0,0 +1,51 @@
+package com.hackybear.hungry_scan_core.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.hackybear.hungry_scan_core.listener.GeneralListener;
+import com.hackybear.hungry_scan_core.utility.TimeRange;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.DayOfWeek;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners({AuditingEntityListener.class, GeneralListener.class})
+@Entity
+@Table(
+        name = "menu_plans",
+        uniqueConstraints = @UniqueConstraint(
+                name = "uk_menu_day",
+                columnNames = {"menu_id", "day_of_week"}
+        )
+)
+public class MenuPlan implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_id", nullable = false)
+    @JsonIgnore
+    private Menu menu;
+
+    @Enumerated(EnumType.STRING)
+    private DayOfWeek dayOfWeek;
+
+    @ElementCollection
+    private Set<TimeRange> timeRanges = new HashSet<>();
+
+}
