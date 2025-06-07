4a9587827b32f2ae0319a19cdb49712e54067916
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-06-07 12:57:47 +0200

    Updated MenuMapper to use new MenuPlanMapper and created updateFromSimpleDTO method

--- src/main/java/com/hackybear/hungry_scan_core/dto/mapper/MenuMapper.java
+++ src/main/java/com/hackybear/hungry_scan_core/dto/mapper/MenuMapper.java
@@ -4,15 +4,22 @@ import com.hackybear.hungry_scan_core.dto.MenuCustomerDTO;
 import com.hackybear.hungry_scan_core.dto.MenuDTO;
 import com.hackybear.hungry_scan_core.dto.MenuSimpleDTO;
 import com.hackybear.hungry_scan_core.entity.Menu;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", uses = {StandardDayPlanMapper.class, TranslatableMapper.class})
+@Mapper(componentModel = "spring",
+        uses = {
+                StandardDayPlanMapper.class,
+                TranslatableMapper.class,
+                MenuPlanMapper.class
+        })
 public interface MenuMapper {
 
     @Mapping(expression = "java(menu.getRestaurant().getId())",
             target = "restaurantId")
     MenuSimpleDTO toSimpleDTO(Menu menu);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromSimpleDTO(MenuSimpleDTO menuSimpleDTO, @MappingTarget Menu menu);
 
     @Mapping(expression = "java(new com.hackybear.hungry_scan_core.entity.Restaurant(menuSimpleDTO.restaurantId()))",
             target = "restaurant")
