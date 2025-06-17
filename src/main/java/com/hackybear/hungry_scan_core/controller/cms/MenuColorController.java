b3ce1a1dc4ad958b4a622c06d4f48209e870f305
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-06-17 17:49:16 +0200

    Created MenuColorController and its test

---
+++ src/main/java/com/hackybear/hungry_scan_core/controller/cms/MenuColorController.java
@@ -0,0 +1,42 @@
+package com.hackybear.hungry_scan_core.controller.cms;
+
+import com.hackybear.hungry_scan_core.controller.ResponseHelper;
+import com.hackybear.hungry_scan_core.dto.MenuColorDTO;
+import com.hackybear.hungry_scan_core.service.interfaces.MenuColorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/cms/menu-colors")
+@RequiredArgsConstructor
+public class MenuColorController {
+
+    private final MenuColorService menuColorService;
+    private final ResponseHelper responseHelper;
+
+    @GetMapping
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<List<MenuColorDTO>> list() {
+        return ResponseEntity.ok(menuColorService.findAll());
+    }
+
+    @PostMapping("/show")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Map<String, Object>> show(@RequestBody Long id) {
+        return responseHelper.getResponseEntity(id, menuColorService::findById);
+    }
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public ResponseEntity<Void> options() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Allow", "GET, POST, OPTIONS");
+        return new ResponseEntity<>(headers, HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/hackybear/hungry_scan_core/controller/cms/MenuColorControllerTest.java
@@ -0,0 +1,82 @@
+package com.hackybear.hungry_scan_core.controller.cms;
+
+import com.hackybear.hungry_scan_core.dto.MenuColorDTO;
+import com.hackybear.hungry_scan_core.test_utils.ApiRequestUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@TestPropertySource(locations = "classpath:application-test.properties")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class MenuColorControllerTest {
+
+    @Autowired
+    ApiRequestUtils apiRequestUtils;
+
+    @Order(1)
+    @Sql("/data-h2.sql")
+    @Test
+    void init() {
+        log.info("Initializing H2 database...");
+    }
+
+    @Test
+    @WithMockUser(roles = {"MANAGER"})
+    void shouldGetAllMenuColors() throws Exception {
+        List<MenuColorDTO> menuColors =
+                apiRequestUtils.fetchAsList(
+                        "/api/cms/menu-colors", MenuColorDTO.class);
+
+        assertEquals(10, menuColors.size());
+        assertTrue(menuColors.stream().anyMatch(menuColor -> "#800000".equals(menuColor.hex())));
+    }
+
+    @Test
+    void shouldNotAllowUnauthorizedAccessToMenuColors() throws Exception {
+        apiRequestUtils.fetchAndExpectForbidden("/api/cms/menu-colors");
+    }
+
+    @Test
+    @WithMockUser(roles = {"CUSTOMER_READONLY"})
+    void shouldShowMenuColorById() throws Exception {
+        MenuColorDTO menuColor =
+                apiRequestUtils.postObjectExpect200("/api/cms/menu-colors/show", 2, MenuColorDTO.class);
+        assertEquals("#003366", menuColor.hex());
+    }
+
+    @Test
+    void shouldNotAllowUnauthorizedAccessToShowMenuColor() throws Exception {
+        apiRequestUtils.postAndExpectForbidden("/api/cms/menu-colors/show", 5);
+    }
+
+    @Test
+    @WithMockUser(roles = {"MANAGER"})
+    void shouldNotShowMenuColorById() throws Exception {
+        Map<String, Object> responseBody =
+                apiRequestUtils.postAndReturnResponseBody(
+                        "/api/cms/menu-colors/show", 78, status().isBadRequest());
+        assertEquals("Kolor menu nie istnieje.", responseBody.get("exceptionMsg"));
+    }
+
+}
