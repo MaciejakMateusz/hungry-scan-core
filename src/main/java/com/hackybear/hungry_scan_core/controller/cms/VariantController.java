8d7f107f16ec706281da509d653566bd8591d323
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-03 17:59:00 +0200

    Recreated VariantController

---
+++ src/main/java/com/hackybear/hungry_scan_core/controller/cms/VariantController.java
@@ -0,0 +1,43 @@
+package com.hackybear.hungry_scan_core.controller.cms;
+
+import com.hackybear.hungry_scan_core.controller.ResponseHelper;
+import com.hackybear.hungry_scan_core.exception.LocalizedException;
+import com.hackybear.hungry_scan_core.service.interfaces.UserService;
+import com.hackybear.hungry_scan_core.service.interfaces.VariantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cms/variants")
+@RequiredArgsConstructor
+public class VariantController {
+
+    private final ResponseHelper responseHelper;
+    private final UserService userService;
+    private final VariantService variantService;
+
+    @PreAuthorize("hasAnyRole('MANAGER', 'ADMIN')")
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        try {
+            Long activeMenuId = userService.getActiveMenuId();
+            return ResponseEntity.ok(variantService.findAllByMenuId(activeMenuId));
+        } catch (LocalizedException e) {
+            return responseHelper.createErrorResponse(e);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public ResponseEntity<Void> options() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Allow", "GET, OPTIONS");
+        return new ResponseEntity<>(headers, HttpStatus.OK);
+    }
+}
