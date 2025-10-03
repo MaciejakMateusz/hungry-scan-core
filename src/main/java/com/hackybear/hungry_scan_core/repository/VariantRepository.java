c15e3e1f657ed3620c1e665fba83432ad3df01c3
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-03 17:58:50 +0200

    Updated VariantRepository

--- src/main/java/com/hackybear/hungry_scan_core/repository/VariantRepository.java
+++ src/main/java/com/hackybear/hungry_scan_core/repository/VariantRepository.java
@@ -1,25 +1,20 @@
 package com.hackybear.hungry_scan_core.repository;
 
+import com.hackybear.hungry_scan_core.entity.MenuItem;
 import com.hackybear.hungry_scan_core.entity.Variant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface VariantRepository extends JpaRepository<Variant, Long> {
 
     List<Variant> findAllByMenuItemIdOrderByDisplayOrder(Long menuItemId);
 
-    @Modifying
-    @Query("UPDATE Variant v SET v.displayOrder = :displayOrder WHERE v.id = :variantId")
-    void updateDisplayOrders(@Param("variantId") Long variantId, @Param("displayOrder") Integer displayOrder);
-
-    @Query("SELECT MAX(v.displayOrder) FROM Variant v WHERE v.menuItem.id = :menuItemId")
-    Optional<Integer> findMaxDisplayOrderByMenuItemId(@Param("menuItemId") Long menuItemId);
+    @Query("SELECT v.menuItem FROM Variant v WHERE v.menuItem.category.menu.id = :menuId")
+    List<MenuItem> findAllByMenuId(@Param("menuId") Long menuId);
 
 }
