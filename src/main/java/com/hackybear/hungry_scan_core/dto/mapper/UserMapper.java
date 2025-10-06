bde698f006497f7aace5d061da395b7c49c858f9
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-06 21:03:55 +0200

    Updated UserMapper

--- src/main/java/com/hackybear/hungry_scan_core/dto/mapper/UserMapper.java
+++ src/main/java/com/hackybear/hungry_scan_core/dto/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package com.hackybear.hungry_scan_core.dto.mapper;
 
 import com.hackybear.hungry_scan_core.dto.RegistrationDTO;
+import com.hackybear.hungry_scan_core.dto.UserProfileDTO;
+import com.hackybear.hungry_scan_core.dto.UserProfileUpdateDTO;
 import com.hackybear.hungry_scan_core.entity.User;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -10,5 +12,15 @@ public interface UserMapper {
     RegistrationDTO toDTO(User user);
 
     User toUser(RegistrationDTO registrationDTO);
+
+    UserProfileDTO toUserProfileDTO(User user);
+
+    User toUser(UserProfileDTO userProfileDTO);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "username", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "repeatedPassword", ignore = true)
+    void updateFromProfileUpdateDTO(UserProfileUpdateDTO userProfileUpdateDTO, @MappingTarget User user);
 
 }
