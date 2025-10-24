10cdeb8765ce387f00a27b3995d1943ccb2427f7
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-24 18:43:10 +0200

    Updated MenuItemViewCountDTO.java

--- src/main/java/com/hackybear/hungry_scan_core/dto/MenuItemViewCountDTO.java
+++ src/main/java/com/hackybear/hungry_scan_core/dto/MenuItemViewCountDTO.java
@@ -4,12 +4,7 @@ import java.io.Serial;
 import java.io.Serializable;
 
 public record MenuItemViewCountDTO(Long id,
-                                   String pl,
-                                   String en,
-                                   String fr,
-                                   String de,
-                                   String es,
-                                   String uk,
+                                   TranslatableDTO name,
                                    Integer viewsCount) implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
