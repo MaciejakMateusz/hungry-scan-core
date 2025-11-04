8f3e0f8dffb79ad11eb5da5c30c33c2f34ae548d
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-04 16:13:32 +0100

    Updated Language.java

--- src/main/java/com/hackybear/hungry_scan_core/enums/Language.java
+++ src/main/java/com/hackybear/hungry_scan_core/enums/Language.java
@@ -1,6 +1,27 @@
 package com.hackybear.hungry_scan_core.enums;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Language {
-    PL,
-    ENG
+    PL("pl"),
+    EN("en"),
+    DE("de"),
+    FR("fr"),
+    ES("es"),
+    UK("uk");
+
+    private final String name;
+
+    Language(String name) {
+        this.name = name;
+    }
+
+    public static final Map<String, Language> NAME_MAP =
+            Stream.of(values()).collect(Collectors.toUnmodifiableMap(
+                    t -> t.name.toUpperCase(),
+                    t -> t
+            ));
+
 }
