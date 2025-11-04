a609ab6dc81b9b3e9da9745ad3446ff3458c7389
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-04 16:14:38 +0100

    Updated SettingsDTO.java

--- src/main/java/com/hackybear/hungry_scan_core/dto/SettingsDTO.java
+++ src/main/java/com/hackybear/hungry_scan_core/dto/SettingsDTO.java
@@ -2,17 +2,26 @@ package com.hackybear.hungry_scan_core.dto;
 
 import com.hackybear.hungry_scan_core.enums.Language;
 import com.hackybear.hungry_scan_core.utility.TimeRange;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.DayOfWeek;
 import java.util.Map;
+import java.util.Set;
 
 public record SettingsDTO(Long id,
                           Long restaurantId,
                           Map<DayOfWeek, TimeRange> operatingHours,
                           Long bookingDuration,
+                          @NotNull
                           Language language,
+
+                          @NotEmpty
+                          @NotNull
+                          Set<Language> supportedLanguages,
+
                           Long employeeSessionTime,
                           Long customerSessionTime,
                           Short capacity,
