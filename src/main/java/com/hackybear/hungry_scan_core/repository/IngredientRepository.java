380d7f60f0d04030dbe2c2cab3247eed36a25096
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-09 21:23:14 +0100

    Updated IngredientRepository.java

--- src/main/java/com/hackybear/hungry_scan_core/repository/IngredientRepository.java
+++ src/main/java/com/hackybear/hungry_scan_core/repository/IngredientRepository.java
@@ -4,6 +4,7 @@ import com.hackybear.hungry_scan_core.entity.Ingredient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -26,4 +27,13 @@ public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
             ORDER BY i.name.pl
             """)
     List<Ingredient> filterByName(@Param("filterValue") String filterValue, @Param("restaurantId") Long restaurantId);
+
+
+    @Modifying
+    @Query(value = "delete from menu_items_ingredients where ingredients_id = :id", nativeQuery = true)
+    void deleteFromIngredientsJoin(@Param("id") Long id);
+
+    @Modifying
+    @Query(value = "delete from menu_items_additional_ingredients where additional_ingredients_id = :id", nativeQuery = true)
+    void deleteFromAdditionalJoin(@Param("id") Long id);
 }
