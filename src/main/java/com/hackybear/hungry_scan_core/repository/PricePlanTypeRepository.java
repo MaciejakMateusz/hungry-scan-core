3c2a3c168cd0e6a7c2f330a49d82210055fc1c12
MaciejakMateusz <maciejakmateusz@gmail.com>
2026-01-22 20:59:24 +0100

    Updated PricePlanTypeRepository.java

--- src/main/java/com/hackybear/hungry_scan_core/repository/PricePlanTypeRepository.java
+++ src/main/java/com/hackybear/hungry_scan_core/repository/PricePlanTypeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PricePlanTypeRepository extends JpaRepository<PricePlanType, String> {
+public interface PricePlanTypeRepository extends JpaRepository<PricePlanType, Long> {
 }
